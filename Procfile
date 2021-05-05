453fd67eefa2eb8183dadb3aaabb0f7f9a849df1
xjati46 <x_jati_46@yahoo.com>
2021-05-05 15:54:24 +0700

    preparing for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bacsweb.wsgi --log-file -
--- bacsweb/settings.py
+++ bacsweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e'
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e')
+# SECRET_KEY = '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
