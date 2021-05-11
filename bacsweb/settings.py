f7735bd12bed05515ed4e5e13b0db04854f9224f
xjati46 <x_jati_46@yahoo.com>
2021-05-11 10:18:22 +0700

    securing django secret key

--- bacsweb/settings.py
+++ bacsweb/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '23+cp=oaxjo4wkl2b&o$mguqj=w!#luuymwn%1v^+fm!f@z8)e')
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
